18cf19ad48bf32653993cdbf8b98d8be4d593860
Abhishek Rai <abhishek_rai@HCL.COM>
2022-02-10 19:30:41 +0530

    Initial Code

---
+++ Movies.API/Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Movies.API.Data;
+
+namespace Movies.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            SeedDatabase(host);
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                  .ConfigureAppConfiguration((hostingContext, config) =>
+                  {
+                      config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                      config.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: false, reloadOnChange: true);
+                     })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+        private static void SeedDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var moviesContext = services.GetRequiredService<MoviesContext>();
+                MoviesContextSeed.SeedAsync(moviesContext);
+            }
+        }
+
+    }
+}
